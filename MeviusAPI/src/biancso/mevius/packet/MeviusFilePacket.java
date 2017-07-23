0d53ce9ff69fb0a57400d43b3aa518d518d45ef0
biancso <flacki_@naver.com>
2017-07-23 23:31:59 +0900

    Why did i?

--- MeviusAPI/src/biancso/mevius/packet/MeviusFilePacket.java
+++ MeviusAPI/src/biancso/mevius/packet/MeviusFilePacket.java
@@ -1,21 +1,13 @@
 package biancso.mevius.packet;
 
 import java.io.File;
-import java.io.IOException;
-
-import biancso.mevius.packet.file.PacketFile;
 
 @SuppressWarnings("serial")
 public class MeviusFilePacket extends MeviusPacket {
-	private final PacketFile file;
+	private byte[] filebytearr;
 
-	@Deprecated
-	public MeviusFilePacket(File file) throws IOException {
-		this.file = new PacketFile(file);
-	}
-
-	public MeviusFilePacket(PacketFile file) {
-		this.file = file;
+	public MeviusFilePacket(File file) {
+		
 	}
 
 }
