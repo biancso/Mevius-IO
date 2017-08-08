4120d0d013895039c9f58ac0d00a8198be7ebf49
LeeJiSoo <flacki_@naver.com>
2017-08-08 17:24:17 +0900

    Updating Mevius to NIO

--- MeviusAPI/src/biancso/mevius/client/MeviusClient.java
+++ MeviusAPI/src/biancso/mevius/client/MeviusClient.java
@@ -1,12 +1,13 @@
 package biancso.mevius.client;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 import java.util.UUID;
 
 import biancso.mevius.handler.ConnectionType;
@@ -15,127 +16,39 @@ import biancso.mevius.packet.MeviusPacket;
 import biancso.mevius.packet.MeviusResponsablePacket;
 import biancso.mevius.packet.events.PacketEventType;
 
-public class MeviusClient extends Thread {
-	private final Socket socket; // Socket
-	private final UUID uuid; // UniqueId
-	private final ObjectInputStream ois; // ObjectInputStream for transfer o-o-t packet
-	private final ObjectOutputStream oos; // ObjectOutputStream for transfer o-o-t packet
+public class MeviusClient {
+	private final SocketChannel sc;
 	private final MeviusHandler handler; // Handler for control packet and connection events
+	private final UUID uuid;
 
 	// USAGE
 	// MeviusClient client = new MeviusClient(InetAddress.getByname("localhost") ,
 	// 3303, new MeviusHandler());
 	// client.start();
-	/**
-	 * 
-	 * @param addr
-	 *            InetAddress
-	 * @param port
-	 *            int
-	 * @param handler
-	 *            MeviusHandler
-	 * @throws IOException
-	 *             throws IOException
-	 * @author biancso
-	 */
-	@Deprecated
-	public MeviusClient(InetAddress addr, int port, MeviusHandler handler) throws IOException {
-		socket = new Socket(addr, port); // Create new socket for addr:port
-		uuid = UUID.randomUUID(); // Generate new random UniqueId
-		oos = new ObjectOutputStream(socket.getOutputStream()); // Create OutputStream from socket outputStream
-		oos.flush();
-		ois = new ObjectInputStream(socket.getInputStream()); // Create InputStream from socket inputStream
-		this.handler = handler; // Init handler
-		this.handler.connection(ConnectionType.CLIENT_CONNECT_TO_SERVER, this); // Call ConnectionEvent
-	}
-
-	public MeviusClient(MeviusAddress addr, MeviusHandler handler) throws IOException {
-		socket = new Socket(addr.getIp(), addr.getPort());
-		uuid = UUID.randomUUID();
-		oos = new ObjectOutputStream(socket.getOutputStream()); // Create OutputStream from socket outputStream
-		oos.flush();
-		ois = new ObjectInputStream(socket.getInputStream()); // Create InputStream from socket inputStream
-		this.handler = handler; // Init handler
-		this.handler.connection(ConnectionType.CLIENT_CONNECT_TO_SERVER, this); // Call ConnectionEvent
-	}
-
-	// Constructor for MeviusServer
-	public MeviusClient(Socket socket, MeviusHandler handler) throws IOException { // ** WARN ** Not for user
-		this.socket = socket;
-		uuid = UUID.randomUUID();
-		oos = new ObjectOutputStream(socket.getOutputStream());
-		oos.flush();
-		ois = new ObjectInputStream(socket.getInputStream());
+	public MeviusClient(InetSocketAddress addr, MeviusHandler handler) throws IOException {
+		this.sc = SocketChannel.open(addr);
+		sc.configureBlocking(false);
 		this.handler = handler;
+		uuid = UUID.randomUUID();
 	}
 
-	public void close() throws IOException {
-		this.socket.close(); // Close
+	public MeviusClient(SocketChannel channel, MeviusHandler handler) {
+		this.sc = channel;
+		this.handler = handler;
+		uuid = UUID.randomUUID();
 	}
 
 	public boolean isClosed() {
-		return this.socket.isClosed(); // Configure out is socket closed
-	}
-
-	public final Socket getSocket() {
-		return this.socket; // get Socket
+		return !sc.isOpen(); // Configure out is socket closed
 	}
 
 	public final UUID getUUID() {
 		return this.uuid; // get UniqueId
 	}
 
-	public void run() {
-		while (!this.isInterrupted()) {
-			try {
-				Object obj = ois.readObject(); // Read Object from ObjectInputStream
-				if (!(obj instanceof MeviusPacket))
-					continue; // If Object isn't MeviusPacket, jump to next Object
-				MeviusPacket packet = (MeviusPacket) obj; // VAR PAcket
-				handler.callEvent(MeviusHandler.getPacketEventInstance(packet, this, PacketEventType.RECEIVE)); // Call
-																												// Packet
-																												// Receive
-																												// Event
-			} catch (ClassNotFoundException | IOException e) {
-				try {
-					disconnect(); // Disconnect on error
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			}
-		}
-	}
-
 	public void disconnect() throws IOException {
-		socket.shutdownInput();
-		socket.shutdownOutput();
-		socket.close();
+		sc.close();
 		handler.connection(ConnectionType.CLIENT_DISCONNECT_FROM_SERVER, this);
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MeviusClient other = (MeviusClient) obj;
-		if (uuid == null) {
-			if (other.uuid != null)
-				return false;
-		} else if (!uuid.equals(other.uuid))
-			return false;
-		return true;
 	}
 
 	public void sendPacket(MeviusPacket packet) throws IOException {
@@ -145,8 +58,7 @@ public class MeviusClient extends Thread {
 			MeviusResponsablePacket responsablePacket = (MeviusResponsablePacket) packet; // VAR ResponsablePacket
 			Class<? extends MeviusResponsablePacket> packetClass = responsablePacket.getClass(); // get packet class
 			try {
-				Method m = packetClass.getSuperclass().getDeclaredMethod("sent", new Class[] {}); // Invoke method with
-																									// Reflection
+				Method m = packetClass.getSuperclass().getDeclaredMethod("sent", new Class[] {});
 				m.setAccessible(true);
 				try {
 					m.invoke(responsablePacket);
@@ -159,19 +71,21 @@ public class MeviusClient extends Thread {
 			} catch (SecurityException e) {
 				e.printStackTrace();
 			}
-			oos.writeObject(responsablePacket); // Send Packet
-			handler.callEvent(MeviusHandler.getPacketEventInstance(responsablePacket, this, PacketEventType.SEND)); // Call
-																													// Packet
-																													// Send
-																													// Event
-			oos.flush(); // Flush
+			sc.write(convert(packet));
+			handler.callEvent(MeviusHandler.getPacketEventInstance(responsablePacket, this, PacketEventType.SEND));
 		} else {
-			oos.writeObject(packet); // send PAcket
-			handler.callEvent(MeviusHandler.getPacketEventInstance(packet, this, PacketEventType.SEND)); // Call Packet
-																											// Send
-																											// Event
-			oos.flush(); // Flush
+			sc.write(convert(packet));
+			handler.callEvent(MeviusHandler.getPacketEventInstance(packet, this, PacketEventType.SEND));
 		}
+	}
+
+	private ByteBuffer convert(Object obj) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(baos);
+		oos.flush();
+		oos.writeObject(obj);
+		oos.flush();
+		return ByteBuffer.wrap(baos.toByteArray());
 	}
 
 	public MeviusHandler getHandler() {
--- MeviusAPI/src/biancso/mevius/server/MeviusServer.java
+++ MeviusAPI/src/biancso/mevius/server/MeviusServer.java
@@ -1,38 +1,56 @@
 package biancso.mevius.server;
 
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 
 import biancso.mevius.client.MeviusClient;
 import biancso.mevius.handler.ConnectionType;
 import biancso.mevius.handler.MeviusHandler;
-import biancso.mevius.server.exceptions.ServerCreateFailException;
 
 public class MeviusServer extends Thread {
-	private final ServerSocket serversocket;
 	private boolean running = false;
+	private final Selector selector;
 	protected MeviusHandler handler;
+	private final ServerSocketChannel ssc;
 
 	// MEVIUS ALPHA
-	public MeviusServer(int port) throws ServerCreateFailException {
-		try {
-			this.serversocket = new ServerSocket(port);
-		} catch (IOException e) {
-			throw new ServerCreateFailException(e.getMessage());
-		}
+	public MeviusServer(int port) throws IOException {
+		this.ssc = ServerSocketChannel.open();
+		this.selector = Selector.open();
+		ssc.configureBlocking(false);
+		ssc.bind(new InetSocketAddress(port));
+		ssc.register(selector, SelectionKey.OP_ACCEPT);
 		handler = new MeviusHandler();
 
 	}
 
 	public void run() {
-		while (running && !isInterrupted()) {
+		while (true) {
 			try {
-				MeviusClient client = new MeviusClient(serversocket.accept(), handler);
-				client.start();
-				handler.connection(ConnectionType.CLIENT_CONNECT_TO_SERVER, client);
+				selector.select();
+				Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+				while (it.hasNext()) {
+					SelectionKey k = it.next();
+					it.remove();
+
+					if (k.isAcceptable()) {
+						accept(k);
+					} else if (k.isReadable()) {
+						read(k);
+					} else if (k.isWritable()) {
+
+					}
+					continue;
+				}
 			} catch (IOException e) {
-				continue;
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
@@ -53,13 +71,16 @@ public class MeviusServer extends Thread {
 		return handler;
 	}
 
-	public void setTimeout(int time) {
+	private void accept(SelectionKey k) {
 		try {
-			serversocket.setSoTimeout(time);
-		} catch (SocketException e) {
+			ServerSocketChannel sc = (ServerSocketChannel) k.channel();
+			SocketChannel channel = sc.accept();
+			channel.configureBlocking(false);
+			channel.register(selector, SelectionKey.OP_READ);
+			System.out.println(channel.socket().getInetAddress().getHostAddress());
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
 }
